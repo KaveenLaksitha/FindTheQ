d62c261901c8098273e325f3a1a6c74a8cf7d6ae
Kaveen LK <kmklaksitha@gmail.com>
2022-10-22 18:10:29 +0530

    added modals

---
+++ app/src/main/java/com/example/findtheq/StationUserView.java
@@ -0,0 +1,14 @@
+package com.example.findtheq;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StationUserView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_station_user_view);
+    }
+}
