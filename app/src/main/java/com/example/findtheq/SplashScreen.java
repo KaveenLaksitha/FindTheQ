f22ae53d96c7f16f5e515e346af1a632b14377f3
Kaveen LK <kmklaksitha@gmail.com>
2022-10-26 14:55:36 +0530

    updated the login screen and added a splash screen

---
+++ app/src/main/java/com/example/findtheq/SplashScreen.java
@@ -0,0 +1,47 @@
+package com.example.findtheq;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 3000;
+
+    Animation topAnim,bottomAnim;
+    ProgressBar splash_prog;
+
+    ImageView imgView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        imgView = findViewById(R.id.imgView);
+        splash_prog = findViewById(R.id.splash_prog);
+
+        imgView.setAnimation(topAnim);
+        splash_prog.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+    }
+}
