d7b0a218162db3349e963c155d2203830c5b70b4
Kaveen LK <kmklaksitha@gmail.com>
2022-10-29 19:01:54 +0530

    added dbHandler

---
+++ app/src/main/java/com/example/findtheq/models/DBHandler/DBHandler.java
@@ -0,0 +1,64 @@
+package com.example.findtheq.models.DBHandler;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHandler extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "localdb";
+    private static final String TABLE_NAME = "user";
+
+    private static final String ID_COL = "id";
+    private static final String EMAIL_COL = "email";
+    private static final String VEHICLE_TYPE_COL = "vehicleType";
+    private static final String JOINED_STATION_ID_COL = "joinedStationID";
+
+    public DBHandler(Context context) {
+        super(context, DB_NAME, null, 1);
+    }
+
+        @Override
+    public void onCreate(SQLiteDatabase db) {
+            String query = "CREATE TABLE " + TABLE_NAME + " ("
+                    + ID_COL + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + EMAIL_COL + " TEXT,"
+                    + VEHICLE_TYPE_COL + " TEXT,"
+                    + JOINED_STATION_ID_COL + " TEXT)";
+
+            db.execSQL(query);
+    }
+
+    public void addNewUser(String email, String vehicleType) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put(EMAIL_COL, email);
+        values.put(VEHICLE_TYPE_COL, vehicleType);
+        values.put(JOINED_STATION_ID_COL, "");
+
+        db.insert(TABLE_NAME, null, values);
+
+        db.close();
+    }
+
+    public void updateUserDetails(String email, String vehicleType, String stationID) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put(EMAIL_COL, email);
+        values.put(VEHICLE_TYPE_COL, vehicleType);
+        values.put(JOINED_STATION_ID_COL, stationID);
+
+        db.update(TABLE_NAME, values, "email=?", new String[]{email});
+        db.close();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+}
